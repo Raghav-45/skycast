6a3c71b70f6c127159f107210d79360064552e47
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-05-14 22:17:01 +0530

    Refactor font imports and update layout class for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter, Nunito_Sans } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const nunito = Nunito_Sans({
   subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+  weight: ['400', '600'],
+  variable: '--font-nunito-sans',
+});
 
 export const metadata: Metadata = {
   title: 'Sky Cast',
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunito.variable} antialiased`}
       >
         {children}
       </body>
