b6a1356b732f99bfb193b5f7d97bc5032ef355d3
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-05-18 01:26:14 +0530

    Update types.ts

--- frontEnd/lib/types.ts
+++ frontEnd/lib/types.ts
@@ -1,7 +1,28 @@
 export interface WeatherResponse {
   latitude: number;
   longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  current_units: {
+    time: string;
+    interval: string;
+    temperature_2m: string;
+    relative_humidity_2m: string;
+    apparent_temperature: string;
+    precipitation: string;
+    weather_code: string;
+    pressure_msl: string;
+    surface_pressure: string;
+    wind_speed_10m: string;
+    wind_direction_10m: string;
+    wind_gusts_10m: string;
+  };
   current: {
+    time: string;
+    interval: number;
     temperature_2m: number;
     relative_humidity_2m: number;
     apparent_temperature: number;
@@ -13,6 +34,17 @@ export interface WeatherResponse {
     wind_direction_10m: number;
     wind_gusts_10m: number;
   };
+  hourly_units: {
+    time: string;
+    temperature_2m: string;
+    relative_humidity_2m: string;
+    wind_speed_10m: string;
+    wind_direction_10m: string;
+    weather_code: string;
+    apparent_temperature: string;
+    precipitation_probability: string;
+    precipitation: string;
+  };
   hourly: {
     time: string[];
     temperature_2m: number[];
@@ -20,6 +52,22 @@ export interface WeatherResponse {
     wind_speed_10m: number[];
     wind_direction_10m: number[];
     weather_code: number[];
+    apparent_temperature: number[];
+    precipitation_probability: number[];
+    precipitation: number[];
+  };
+  daily_units: {
+    time: string;
+    weather_code: string;
+    temperature_2m_max: string;
+    temperature_2m_min: string;
+    sunrise: string;
+    sunset: string;
+    uv_index_max: string;
+    precipitation_sum: string;
+    precipitation_probability_max: string;
+    wind_speed_10m_max: string;
+    wind_direction_10m_dominant: string;
   };
   daily: {
     time: string[];
@@ -29,9 +77,11 @@ export interface WeatherResponse {
     sunrise: string[];
     sunset: string[];
     uv_index_max: number[];
+    precipitation_sum: number[];
     precipitation_probability_max: number[];
+    wind_speed_10m_max: number[];
+    wind_direction_10m_dominant: number[];
   };
-  timezone: string;
 }
 
 export interface WeatherData {
